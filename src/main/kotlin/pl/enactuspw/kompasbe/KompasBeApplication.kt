f6845b309fba110bcacbd672e56b6137f790d2b1
Aleksander Kapera <aleksander.kapera@besidethepark.com>
2022-05-13 15:31:11 +0200

    Initial application setup

---
+++ src/main/kotlin/pl/enactuspw/kompasbe/KompasBeApplication.kt
@@ -0,0 +1,11 @@
+package pl.enactuspw.kompasbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KompasBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KompasBeApplication>(*args)
+}
---
+++ src/test/kotlin/pl/enactuspw/kompasbe/KompasBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.enactuspw.kompasbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KompasBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
